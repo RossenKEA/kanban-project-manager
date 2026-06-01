b3d9276ae2f4a51f701df48375a7d3daadd1c2b5
RossenKEA <raro0002@stud.kea.dk>
2026-06-01 16:22:13 +0200

    create mock-board

---
+++ src/data/mock-board.ts
@@ -0,0 +1,36 @@
+export const mockColumns = [
+    {
+        id: "todo",
+        title: "Todo",
+        tasks: [
+            {
+                id: "1",
+                title: "Create project structure",
+            },
+            {
+                id: "2",
+                title: "Design board UI",
+            },
+        ],
+    },
+    {
+        id: "progress",
+        title: "In Progress",
+        tasks: [
+            {
+                id: "3",
+                title: "Build Kanban page",
+            },
+        ],
+    },
+    {
+        id: "done",
+        title: "Done",
+        tasks: [
+            {
+                id: "4",
+                title: "Set up Next.js",
+            },
+        ],
+    },
+];
